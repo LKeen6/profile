864d4f8cb214dee3b49d233e7a097bf9da5f5765
LKeen6 <117418151+LKeen6@users.noreply.github.com>
2025-01-14 20:20:31 -0700

    update projects

--- landon-website/components/WorkSlider.js
+++ landon-website/components/WorkSlider.js
@@ -4,61 +4,46 @@ export const workSlider = {
     {
       images: [
         {
-          title: 'title',
+          title: 'Card Layout',
           path: '/thumb1.jpg',
+          link: 'https://film-search-udim.onrender.com/'
         },
         {
-          title: 'title',
+          title: 'Animated Cards',
           path: '/thumb2.jpg',
+          link: 'https://card-layout.onrender.com'
         },
         {
-          title: 'title',
+          title: 'Your Future',
           path: '/thumb3.jpg',
+          link: 'https://css-cards.onrender.com'
         },
         {
-          title: 'title',
+          title: 'Mapty',
           path: '/thumb4.jpg',
+          link: 'https://mapty-nl5z.onrender.com/'
         },
       ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
+    }
   ],
 };
 
-//import swiper react components
-import {Swiper, SwiperSlide} from 'swiper/react';
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-//import swiper styles
+// import swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-//import required modules
-import {Pagination} from 'swiper';
+// import required modules
+import { Pagination } from 'swiper';
 
-//icons
-import {BsArrowRight} from 'react-icons/bs';
+// icons
+import { BsArrowRight } from 'react-icons/bs';
 // next image
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const WorkSlider = () => {
   return (
@@ -69,44 +54,43 @@ const WorkSlider = () => {
           clickable: true,
         }}
         modules={[Pagination]}
-        className='h-[280px] sm:h-[480px]'
+        className="h-[280px] sm:h-[480px]"
       >
-        {workSlider.slides.map((slide, index) => {
-          return (
-            <SwiperSlide key={index}>
-              <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
-                {slide.images.map((image, index) => {
-                  return (
-                    <div className='relative rounded-lg overflow-hidden flex items-center justify-center group' key={index}>
-                      <div className='flex items-center justify-center relative overflow-hidden group'>
-                        {/* image */}
-                          <Image src={image.path} width={500} height={300} alt=''/>
-                        {/* overlay gradient */}
-                          <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#46dbd4] to-[#a39968] opacity-0 group-hover:opacity-80 transition-all duration-700'>
+        {workSlider.slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => (
+                <Link href={image.link} key={index}>
+                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                    <div className="flex items-center justify-center relative overflow-hidden group">
+                      <div className="relative w-[500px] h-[250px]">
+                      {/* image */}
+                      <Image src={image.path} alt={image.title} fill unoptimized  />
+                      </div>
+                      {/* overlay gradient */}
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#46dbd4] to-[#a39968] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      {/* title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          {/* title part 1 */}
+                          <div className="sm:text-sm delay-100">{image.title}</div>
+                          {/* title part 2 */}
+                          <div className="sm:text-sm translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            demo
                           </div>
-                        {/* title */}
-                          <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
-                            <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-
-                              {/* title part 1 */}
-                              <div className='sm:text-sm delay-100'>LIVE</div>
-
-                              {/* title part 2 */}
-                              <div className='sm:text-sm translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
-
-                              {/* icon */}
-                              <div className='sm:text-sm text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight/></div>
-                            </div>
+                          {/* icon */}
+                          <div className="sm:text-sm text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
                           </div>
+                        </div>
                       </div>
                     </div>
-                  )
-                })}
-              </div>
-            </SwiperSlide>
-          );
-        })}
-        
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
