34febc4f2d02e8f97791a5e720f9d7a4589bc6bb
LKeen6 <117418151+LKeen6@users.noreply.github.com>
2025-02-03 20:41:52 -0700

    update socials

--- landon-website/components/Socials.js
+++ landon-website/components/Socials.js
@@ -2,35 +2,31 @@ import Link from 'next/link';
 
 //icons
 import {
-  RiInstagramLine,
-  RiFacebookLine,
-  RiDribbbleLine,
-  RiBehanceLine,
-  RiPinterestLine,
+  RiLinkedinBoxFill,
+  RiGithubFill,
+  RiFacebookBoxFill,
+  RiInstagramFill,
 } from 'react-icons/ri';
 
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-xl'> {/* You can adjust text-xl to any fixed size */}
-      <Link href={''} className='hover:text-[#46dbd4] transition-all duration-300'>
-        <RiInstagramLine className="text-3xl" /> {/* Fixed icon size */}
+      <Link href='https://www.instagram.com/foxylandobae/#' className='hover:text-[#46dbd4] transition-all duration-300'>
+        <RiInstagramFill className="text-3xl" /> {/* Fixed icon size */}
       </Link>
 
-      <Link href={''} className='hover:text-[#46dbd4] transition-all duration-300'>
-        <RiFacebookLine className="text-3xl" />
+      <Link href='https://www.facebook.com/landon.keen.9' className='hover:text-[#46dbd4] transition-all duration-300'>
+        <RiFacebookBoxFill className="text-3xl" />
       </Link>
 
-      <Link href={''} className='hover:text-[#46dbd4] transition-all duration-300'>
-        <RiDribbbleLine className="text-3xl" />
+      <Link href='https://github.com/LKeen6' className='hover:text-[#46dbd4] transition-all duration-300'>
+        <RiGithubFill className="text-3xl" />
       </Link>
 
-      <Link href={''} className='hover:text-[#46dbd4] transition-all duration-300'>
-        <RiBehanceLine className="text-3xl" />
+      <Link href='https://www.linkedin.com/in/landon-keen' className='hover:text-[#46dbd4] transition-all duration-300'>
+        <RiLinkedinBoxFill className="text-3xl" />
       </Link>
 
-      <Link href={''} className='hover:text-[#46dbd4] transition-all duration-300'>
-        <RiPinterestLine className="text-3xl" />
-      </Link>
     </div>
   );
 };
